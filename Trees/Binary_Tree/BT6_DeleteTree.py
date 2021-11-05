d61c50daa7b8d52d2cebd28ec23b0fb699b9028b
Aman6233 <46619754+Aman6233@users.noreply.github.com>
2021-11-05 18:09:06 +0530

    Delete Trees directory

--- Trees/Binary_Tree/BT6_DeleteTree.py
+++
@@ -1,9 +0,0 @@
-#Problem-9: Delete the Binary Tree
-
-def deleteBinaryTree(root):
-	if root==None:
-		return
-
-	deleteBinaryTree(root.left)
-	deleteBinaryTree(root.right)
-	del root
--- Trees/Generic_Tree/Generic_tree.py
+++
@@ -1,9 +0,0 @@
-#Node of a Generic tree
-
-class GenericTreeNode:
-	def __init__(self, data = None):
-		self.data = data
-		self.firstChild = None
-		self.nextSibling = None
-
-# We can treat all generic trees with a first child/next sibling representation as binary trees.
