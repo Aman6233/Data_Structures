b369c4fc5793b81f3702d1be8568203a31a73b3b
Aman6233 <46619754+Aman6233@users.noreply.github.com>
2022-03-13 13:49:22 +0530

    Add files via upload

---
+++ Linked_List/Circular_LinkedList.py
@@ -0,0 +1,21 @@
+#Circular linked list
+
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+	def setData(self, data):
+		self.data = data
+
+	def getData(self):
+		return self.data
+
+	def setNext(self, next):
+		self.next = next
+
+	def getNext(self):
+		return self.next
+
+	def hasNext(self):
+		return self.next != None
